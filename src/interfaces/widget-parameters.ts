2fab1550ebd7c02c09a28258f2c3339524967c05
George Goshanov <goshancode@gmail.com>
2022-02-17 16:26:39 +0300

    rename orderId to merchantOrderId

--- src/interfaces/widget-parameters.ts
+++ src/interfaces/widget-parameters.ts
@@ -1,11 +1,23 @@
+/**
+ * **Check full documentation here**
+ *
+ * https://kikimora-labs.notion.site/NearPay-Widget-Documentation-for-Merchants-fbf29ddaf92d4ea190ad92aef4d90474
+ *
+ * `toCurrency` - Provide crypto ticker for example `NEAR` if you want to lock receiving currency, customer won't be able to change that
+ *
+ * `toAmount` - Only available if `toCurrency` is provided, you may also lock amount
+ *
+ * `toWallet` - Only available if `toCurrency` is provided, this wallet will receive all funds
+ *
+ * `merchantOrderId` - Custom string, your order identifier to help you track orders.
+ *
+ * `apiKey` - You merchant public key, NOT A SECRET KEY
+ */
 export interface WidgetParams {
-  // cannot exist without each other
   toCurrency?: string;
   toAmount?: string;
-  // maybe set separately
   toWallet?: string;
-  // unique order identifier, used in payment flow
-  orderId?: string;
+  merchantOrderId?: string;
   apiKey?: string;
 }
 
