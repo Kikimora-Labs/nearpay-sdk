721d364058a321ad005589509178337e7f464674
truepatch <truepatch@gmail.com>
2022-10-16 11:32:16 +0500

    refactor: preconfiguration

--- dist/helpers/getWidgetUrl.d.ts
+++
@@ -1,9 +0,0 @@
-import { EnvironmentMode } from '../interfaces/environment';
-import { SignedWidgetParams } from '../interfaces/widget-parameters';
-/**
- *
- * @param environment switch between production/development versions of widget
- *
- * @returns `url` - ready to be inserted into `iframe.src` attribute
- */
-export declare const getWidgetUrl: (environment: EnvironmentMode, params?: SignedWidgetParams | undefined) => string;
--- dist/helpers/makeSignatureString.d.ts
+++
@@ -1,6 +0,0 @@
-import { WidgetParams } from '../interfaces/widget-parameters';
-/**
- * @param params Pass your merchant parameters without `signature`
- * @returns `string` which you should hash using your `secret_key`
- */
-export declare function makeSignatureString(params: WidgetParams): string;
