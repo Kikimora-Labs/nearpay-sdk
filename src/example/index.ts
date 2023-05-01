1d45ca296eefa68b20a790074b72359d2cd0fead
George Goshanov <george@kikimora.tech>
2023-05-01 14:14:52 +0100

    clear example from params

--- src/example/index.ts
+++ src/example/index.ts
@@ -10,22 +10,9 @@ const container = document.querySelector(
   '#nearpay-widget-container',
 ) as HTMLElement;
 
-/**
- ?toWallet=0xd5c6A645073828A651b3ff0d4d42E08439D3178d&toCurrency=USDT-MATIC&toAmount=10&merchantOrderId=e7699166-c9d1-4ce1-8198-58c571d26bc1&apiKey=6170fa03-5dcd-42ea-bef6-b3eab64cf6b5&email=teke97%40gmail.com&signature=d082ddc3faf50a35a1ffb718ac21d7775a891df4a90079a5b4409718ec0bc9b7"
- */
 const widget = new NearPay({
   mountElement: container,
   environment: 'development',
-  params: {
-    email: 'teke97@gmail.com',
-    toAmount: '10',
-    toWallet: '0xd5c6A645073828A651b3ff0d4d42E08439D3178d',
-    toCurrency: 'USDT-MATIC',
-    merchantOrderId: 'e7699166-c9d1-4ce1-8198-58c571d26bc1',
-    apiKey: '6170fa03-5dcd-42ea-bef6-b3eab64cf6b5',
-    signature:
-      'd082ddc3faf50a35a1ffb718ac21d7775a891df4a90079a5b4409718ec0bc9b7',
-  },
 });
 
 const listener = (event: OnLoadedEvent) => {
